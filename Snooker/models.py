f76e85c9232e42c012f3fbdcabb10e97e5ebedc6
Sampo Verkasalo <sampo.verkasalo@gmail.com>
2012-12-13 15:54:50 +0200

    calculation frame score

--- Snooker/models.py
+++ Snooker/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 from positions import PositionField
-
+from django.db.models import Sum
 
 class Player(models.Model):
     pass
 
 
 class Frame(models.Model):
-    pass
+    player1 = models.ForeignKey(Player, related_name='frames_1')
+    player2 = models.ForeignKey(Player, related_name='frames_2')
+
+    def get_score(self):    
+        strikes = self.strike_set
+        p1 = self.player1
+        p2 = self.player2
+        p1_sum = strikes.filter(player=p1).aggregate(Sum('points')).get('points__sum')
+        p2_sum = strikes.filter(player=p2).aggregate(Sum('points')).get('points__sum')
+        return [p1_sum, p2_sum]
 
 
 class Strike(models.Model):
--- Snooker/tests.py
+++ Snooker/tests.py
@@ -6,18 +6,22 @@ class StrikeModelTest(TestCase):
     def setUp(self):
         self.strike = Strike()
         self.strike.points = 1
-        self.player = Player()
-        self.player.save()
+        self.player1 = Player()
+        self.player1.save()
+        self.player2 = Player()
+        self.player2.save()
         self.frame = Frame()
+        self.frame.player1 = self.player1
+        self.frame.player2 = self.player2
         self.frame.save()
-        self.strike.player = self.player
+        self.strike.player = self.player1
         self.strike.frame = self.frame
         self.strike.save()
 
     def test_simple_get_from_db(self):
         db_strike = Strike.objects.get(id=self.strike.id)
         self.assertEquals(db_strike.points, 1)
-        self.assertEquals(db_strike.player.id, self.player.id)
+        self.assertEquals(db_strike.player.id, self.player1.id)
         self.assertEquals(db_strike.frame.id, self.frame.id)
         self.assertEquals(db_strike.position, 0)
 
@@ -31,8 +35,25 @@ class PlayerModelTest(TestCase):
 class FrameModelTest(TestCase):
     def setUp(self):
         self.frame = Frame()
+        player1 = Player()
+        player1.save()
+        player2 = Player()
+        player2.save()
+        self.frame.player1 = player1
+        self.frame.player2 = player2
         self.frame.save()
 
     def test_simple_get_from_db(self):
         db_frame = Frame.objects.get(id=self.frame.id)
         self.assertEquals(db_frame.id, self.frame.id)
+        self.assertEquals(db_frame.player1.id, self.frame.player1.id)
+        self.assertEquals(db_frame.player2.id, self.frame.player2.id)
+
+    def test_get_frame_score(self):
+        s1 = Strike()
+        s1.frame = self.frame
+        s1.player = self.frame.player1
+        s1.points = 5
+        s1.save()
+        score = self.frame.get_score()
+        self.assertEquals(score[0], 5)
