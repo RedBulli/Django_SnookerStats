290a4f0c12ff24385cd6d86f99d87d2202da1e58
Sampo Verkasalo <sampo.verkasalo@gmail.com>
2012-12-13 02:10:13 +0200

    added frame and player models

--- Snooker/models.py
+++ Snooker/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
+class Player(models.Model):
+  pass
+
+class Frame(models.Model):
+  pass
+
 class Strike(models.Model):
+  order = models.IntegerField()
   points = models.IntegerField()
+  frame = models.ForeignKey(Frame)
+  player = models.ForeignKey(Player)
--- Snooker/tests.py
+++ Snooker/tests.py
@@ -1,10 +1,33 @@
 from django.test import TestCase
-from models import Strike
+from models import Strike, Player, Frame
 
 class StrikeModelTest(TestCase):
   def test_creation_and_saving(self):
     strike = Strike()
     strike.points = 1
+    player = Player()
+    player.save()
+    frame = Frame()
+    frame.save()
+    strike.player = player
+    strike.frame = frame
+    strike.order = 1
     strike.save()
     db_strike = Strike.objects.get(id=strike.id)
     self.assertEquals(db_strike.points, 1)
+    self.assertEquals(db_strike.player.id, player.id)
+    self.assertEquals(db_strike.frame.id, frame.id)
+    self.assertEquals(db_strike.order, 1)
+
+class PlayerModelTest(TestCase):
+  def test_creation_and_saving(self):
+    player = Player()
+    player.save()
+
+class FrameModelTest(TestCase):
+  def test_creation_and_saving(self):
+    frame = Frame()
+    frame.save()
+
+  def test_next_order_no(self):
+    pass
