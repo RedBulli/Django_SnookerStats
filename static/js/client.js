d82201649238943a330a460d810605b68d6b2b63
Sampo Verkasalo <sampo.verkasalo@gmail.com>
2013-02-10 19:48:45 +0200

    client change

--- static/js/client.js
+++ static/js/client.js
@@ -3,34 +3,6 @@ var currentMatch;
 var matches;
 var currentFrame;
 
-var addSlash = function( str ) {
-  return str + ( ( str.length > 0 && str.charAt( str.length - 1 ) === '/' ) ? '' : '/' );
-}
-
-Backbone.Model.prototype.url = function () {
-  // Use the id if possible
-  var url = this.id;
-  
-  // If there's no idAttribute, use the 'urlRoot'. Fallback to try to have the collection construct a url.
-  // Explicitly add the 'id' attribute if the model has one.
-  if ( !url ) {
-    url = this.urlRoot;
-    url = url || this.collection && ( _.isFunction( this.collection.url ) ? this.collection.url() : this.collection.url );
-
-    if ( url && this.has( 'id' ) ) {
-      url = addSlash( url ) + this.get( 'id' );
-    }
-  }
-
-  url = url && addSlash( url );
-  if (url) {
-    if (url[0] === '/') {
-      url = ROOT + url;
-    }
-  }
-  return url || null;
-}
-
 $(document).ready(function() {
   players = new Players();
   players.fetch({dataType: 'jsonp', success: matchFetch});
